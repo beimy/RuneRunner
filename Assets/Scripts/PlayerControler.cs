f78fcd8eb31b8acbd1f1a244e91bdda5f62c585d
MergeMastaFlash <arbm159357@gmail.com>
2021-01-09 19:27:21 -0500

    Push from Laptop

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    [SerializeField]
+    public Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            OnClick();
+    }
+
+    void OnClick()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Transform objectHit = hit.transform;
+            
+
+            // Do something with the object that was hit by the raycast.
+            Debug.Log("object hit = " + hit.collider.gameObject.name as string);
+        }
+    }
+}
